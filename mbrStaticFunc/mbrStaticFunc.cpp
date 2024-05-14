9b46ddc06f7a0934e60831a3b78977c2223d74f4
galang48 <galangyudha1945@gmail.com>
2024-05-14 10:45:03 +0700

    menambahlan akses private dan public di class mahasiswa

---
+++ mbrStaticFunc/mbrStaticFunc.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa {
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) { nim = pNim; /*Definisi Function*/ }
+    static int getNim() { return nim; /*Definisi Function*/ }
+
+    Mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
