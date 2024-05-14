1d29a5bba3fec89517296fa4d37ae3d48b25a725
galang48 <galangyudha1945@gmail.com>
2024-05-14 13:03:21 +0700

    definisi member function

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -11,6 +11,20 @@ public:
     void cetakData();
     void isiData();
 };
+//Definisi member Function
+angka::angka(int i){ //Construction
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+angka::~angka() {
+    cout << endl;
+    cetakData();
+    delete[]arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
 int main()
 {
     
